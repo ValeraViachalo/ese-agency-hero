aa04e73a71fbee24b2dddbcd69b8cefcf90a5364
Valerii <valeraviachalo@gmail.com>
2024-03-07 15:36:59 +0200

    version 2 (created array w/ images insted of fetching every time)

--- src/pages/Home/Hero/Hero.jsx
+++ src/pages/Home/Hero/Hero.jsx
@@ -7,31 +7,45 @@ import { ScrollTrigger } from "gsap/all";
 
 export default function Hero() {
   const images = Array.from({length: 49}, (_, i) => `/images/hero/ese-hero-sequence${i + 1}.webp`);
-  const frameRate = 9;
+  const frameRate = 12;
   
   const [currentFrame, setCurrentFrame] = useState(0);
   const [animationEnded, setAnimationEnded] = useState(false);
+  const [loadedImages, setLoadedImages] = useState([]);
 
   const imageRef = useRef();
+
+  // Preload images
+  useEffect(() => {
+    const loadImages = images.map(image => {
+      const img = new Image();
+      img.src = image;
+      return img;
+    });
+    setLoadedImages(loadImages);
+  }, [images]);
 
   useEffect(() => {
     if(!animationEnded) {
       const timer = setInterval(() => {
-        if (currentFrame + 1 === images.length) {
-          setAnimationEnded(true);
-          clearInterval(timer);
-        } else {
-          setCurrentFrame(currentFrame + 1);
-        }
+        setCurrentFrame(prevFrame => {
+          if (prevFrame + 1 === images.length) {
+            setAnimationEnded(true);
+            clearInterval(timer);
+            return prevFrame;
+          } else {
+            return prevFrame + 1;
+          }
+        });
       }, 1000 / frameRate);
   
       return () => clearInterval(timer);
     }
-  }, [currentFrame, images.length, frameRate, animationEnded]);
+  }, [images.length, frameRate, animationEnded]);
 
   gsap.registerPlugin(ScrollTrigger)
   
-  useGSAP(() => {
+  useEffect(() => {
     if (imageRef.current && animationEnded) {
       ScrollTrigger.create({
         trigger: '.hero',
@@ -45,12 +59,11 @@ export default function Hero() {
           }
         });
     }
-  }, [imageRef, animationEnded])
+  }, [imageRef, animationEnded, images.length])
 
- 
   return (
     <main className="hero">
-      <img ref={imageRef} src={images[currentFrame]} alt="animation frame" className="hero__image"/>
+      <img ref={imageRef} src={loadedImages[currentFrame]?.src} alt="animation frame" className="hero__image"/>
     </main>
   );
 }
